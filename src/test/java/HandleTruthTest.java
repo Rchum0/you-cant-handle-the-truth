491eff175ad3f84e2f242e65870db8cf94226b4b
Chum <ravichum@gmail.com>
2020-11-21 17:56:56 -0800

    HandleTruth first commit

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,8 +1,62 @@
+import com.sun.source.tree.Tree;
 import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
 
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
-        HandleTruth.wordCount();
+        //HandleTruth.wordCount();
     }
+
+    public void finalTest() {
+        TreeMap<Integer, Set<String>> actual;
+        TreeMap<Integer, Set<String>> expected = new TreeMap<>();
+        int num = 5;//input for switch
+        Queue key = new LinkedList();
+
+        while (num > 0) {
+            actual = HandleTruth.wordCount(test(num, expected, key));
+            assertEquals(actual.size(), expected.size());
+            num--;
+            while (key.size() > 0) {
+                int keys = (int) key.poll();
+                assertEquals(actual.get(keys), expected.get(keys));
+            }
+            expected = new TreeMap<>();
+        }
+    }
+
+    private static String test(int num, TreeMap<Integer, Set<String>> expected, Queue key){
+        Set<String> set = new HashSet<>();
+        String text;
+        switch (num){
+            case 1:
+                text = "hello ";
+                set = new HashSet<>();
+                set.add("hello");
+                break;
+            case 2:
+                text = "dog";
+                set.add("dog");
+                expected.put(1,set);
+                key.add(1);
+                break;
+            case 3:
+                text = "hi hi hi hi hi hi hi";
+                set = new HashSet<>();
+                set.add("hi");
+                expected.put(7,set);
+                key.add(7);
+                set = new HashSet<>();
+                set.add("hi");
+                expected.put(7,set);
+                key.add(7);
+
+
+        }
+    }
+
 }
